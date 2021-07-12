23069b6d29cdd9d92258a964906062a9162d85d8
LofizDev <86564838+LofizDev@users.noreply.github.com>
2021-07-12 15:08:59 +0700

    Update i18n.jsx

--- src/Components/i18n/i18n.jsx
+++ src/Components/i18n/i18n.jsx
@@ -7,7 +7,7 @@ import { initReactI18next } from "react-i18next";
 const resources = {
   vn: {
     translation: {
-      "SỐLIỆU":"SỐ LIỆU",
+      "SỐLIỆU":"SỐ LIỆUUU",
       "LCQG": 'Lựa chọn quốc gia',
       "SCM": "Số ca nhiễm",
       "SCK": "Số ca khỏi",
